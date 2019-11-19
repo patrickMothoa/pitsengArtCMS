da2b36c1b9857fa75f0a6649d010f8cd0279fd0d
Msi87 <51952763+Msi87@users.noreply.github.com>
2019-11-19 15:24:29 +0200

    c

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { WindowService } from '../window.service';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-register',
@@ -9,50 +7,9 @@ import * as firebase from 'firebase';
 })
 export class RegisterPage implements OnInit {
 
-  windowRef: any;
-
-  // phoneNumber = new PhoneNumber()
-
-  verificationCode: string;
-
-  user: any;
-
-  constructor(/*private win: WindowService*/) { }
+  constructor() { }
 
   ngOnInit() {
-    // this.windowRef = this.win.windowRef
-    this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container')
-
-    this.windowRef.recaptchaVerifier.render()
   }
 
-
-  sendLoginCode() {
-
-    const appVerifier = this.windowRef.recaptchaVerifier;
-
-    // const num = this.phoneNumber.e164;
-
-    // firebase.auth().signInWithPhoneNumber(num, appVerifier)
-  //           .then(result => {
-
-  //               this.windowRef.confirmationResult = result;
-
-  //           })
-  //           .catch( error => console.log(error) );
-
-  // }
-
-  // verifyLoginCode() {
-  //   this.windowRef.confirmationResult
-  //                 .confirm(this.verificationCode)
-  //                 .then( result => {
-
-  //                   this.user = result.user;
-
-  //   })
-  //   .catch( error => console.log(error, "Incorrect code entered?"));
-  // }
-
-  }
 }
