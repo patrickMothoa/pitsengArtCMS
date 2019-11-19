d22360692c5741f276c5d207b7eedc6d3ccba73c
449sharon <50619633+449sharon@users.noreply.github.com>
2019-11-19 11:38:11 +0200

    i have connected app with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import * as firebase from 'firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1es8iP2ZqtEUH8TJCa9HRDU7yjgRcuqs",
+  authDomain: "pitsengart.firebaseapp.com",
+  databaseURL: "https://pitsengart.firebaseio.com",
+  projectId: "pitsengart",
+  storageBucket: "pitsengart.appspot.com",
+  messagingSenderId: "381927677673",
+  appId: "1:381927677673:web:0c7a2746479e8fda33da78",
+  measurementId: "G-3KKJYT38C2"
+};
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 @NgModule({
   declarations: [AppComponent],
