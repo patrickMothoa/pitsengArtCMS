3d1756d4e43302457dd2d789b18109d29ee85118
Msi87 <51952763+Msi87@users.noreply.github.com>
2019-11-25 13:48:03 +0200

    dd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { ProductService } from '../services/product.service';
+import { DataService } from "../services/data.service";
+import { FormControl } from "@angular/forms";
+import { debounceTime } from "rxjs/operators";
+// import { NavController } from 'ionic-angular';
+// import 'rxjs/add/operator/debounceTime';
 
 
 @Component({
@@ -9,8 +14,11 @@ import { ProductService } from '../services/product.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  // public searchTerm: string = "";
+  // public items: any;
+    searchControl: FormControl;
   db = firebase.firestore();
   event = {
     image: '',
@@ -39,36 +47,23 @@ export class HomePage {
     centeredSlides: true
   };
 
-  constructor(private router: Router, public productService: ProductService) {}
-
-  ngOnInit() {
-   // this.ViewDetails(this.navDetails)
-    this.getProducts();
-
-    // firebase.auth().onAuthStateChanged(category => {
-    //   if (category) {
-    //      this.event.categories = this.event.productno
-    //   this.getProducts();
-    //   } else {
-    //        console.log("not here");  
-    //   }
-    // })
+  constructor(private dataService: DataService, private router: Router, public productService: ProductService) {
+    this.searchControl = new FormControl();
   }
 
-//   ViewDetails(view) {
-//     this.navDetails.push(this.ViewDetails,{
-//       image: view.image,
-//       categories:view.categories,
-//       name:view.name,
-//       price:view.price,
-//       productno:view.productno,
-//       desc: view.desc,
-//       small:view.small,
-//       medium:view.medium,
-//       large:view.large
-// });
-//     this.router.navigateByUrl('/details')
-//   }
+  ngOnInit() {
+    this.getProducts();
+    this.setFilteredItems("");
+
+    this.searchControl.valueChanges
+    .pipe(debounceTime(700))
+    .subscribe(search => {
+      this.setFilteredItems(search);
+    });
+  }
+  setFilteredItems(searchTerm) {
+    this.items = this.dataService.filterItems(searchTerm);
+  }
 
   openProfile(){
     this.router.navigateByUrl('/profile');
@@ -109,17 +104,30 @@ export class HomePage {
   customErrorMsg:boolean = false;
   listFetchStatus:boolean = false;
 
- 
-  onSearchInput(){
+  
+  // onSearchInput(){
     
-    if(this.searchTerm.length>0){
-     //  this.event = this.productService.filterItems(this.searchTerm);
-       this.searching = true;
+  //   if(this.searchTerm.length>0){
+  //     // this.event = this.productService.filterItems(this.searchTerm);
+  //      this.searching = true;
        
-    }else{
-      this.searching = false;
-    }
-  }
+  //   }else{
+  //     this.searching = false;
+  //   }
+  // }
+  ionViewDidLoad() {
+
+    // this.setFilteredItems();
+
+    // this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+
+    //     this.searching = false;
+    //     this.setFilteredItems();
+
+    // });
+
+
+}
 
   select(item){
     this.searchTerm = item.title;
@@ -128,32 +136,6 @@ export class HomePage {
   }
 
 
-
-    /// retrieving from database
-
-  // getProducts(){
-  //   this.db.collection('Products').get().then(snapshot => {
-  //     if (snapshot.empty) {
-  //       this.myProduct = false;
-  //     } else {
-  //       this.myProduct = true;
-  //       snapshot.forEach(doc => {
-  //         this.event.image= doc.data().image;
-  //         this.event.categories = doc.data().categories
-  //         this.event.name=doc.data().name
-  //         this.event.price=doc.data().price
-  //         this.event.productno=doc.data().productno
-  //         this.event.desc=doc.data().desc
-  //         this.event.small=doc.data().small
-  //         this.event.medium=doc.data().medium
-  //         this.event.large=doc.data().large
-          
-  //       })
-  //     }
-  //   })
-  // }
-
-      // retriving from firebase.firestore
       getProducts() {
         this.db.collection('Products').get().then(snapshot => {
           if (snapshot.empty) {
