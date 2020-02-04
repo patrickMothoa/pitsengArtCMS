cfa6c521ef092bc8ff78e7e656813d43a574ce36
kiwokuhle <39264305+kiwokuhle@users.noreply.github.com>
2020-02-04 10:55:02 +0200

    navigation queries page ui updates

--- src/app/components/popover/popover.component.ts
+++ src/app/components/popover/popover.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController, ModalController } from '@ionic/angular';
 import { ProfilePage } from 'src/app/pages/profile/profile.page';
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-popover',
@@ -10,7 +12,8 @@ import { ProfilePage } from 'src/app/pages/profile/profile.page';
 export class PopoverComponent implements OnInit {
 
   constructor(   public modalController: ModalController,  
-    public popoverController: PopoverController,) { }
+    public popoverController: PopoverController,
+    private router: Router,) { }
 
   ngOnInit() {}
   async createProfile() {
@@ -25,6 +28,14 @@ export class PopoverComponent implements OnInit {
   DismissClick(){
     this.popoverController.dismiss({
       'dismissed':true
+    });
+  }
+  logOut(){
+    firebase.auth().signOut().then(()=> {
+      // Sign-out successful.
+      this.router.navigateByUrl('/login');
+    }).catch((error)=> {
+      // An error happened.
     });
   }
 }
