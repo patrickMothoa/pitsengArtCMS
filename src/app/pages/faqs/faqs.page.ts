ae1ab1469495c03529ede71631534b0949bd8b43
449sharon <50619633+449sharon@users.noreply.github.com>
2020-02-11 15:54:13 +0200

    updates

--- src/app/pages/faqs/faqs.page.ts
+++ src/app/pages/faqs/faqs.page.ts
@@ -17,6 +17,30 @@ export class FaqsPage implements OnInit {
     question2: '',
     answer2: ''
   }
+  refundQues = {
+    question1: '',
+    answer1: ''
+  }
+  refunds1Ques = {
+   question2: '',
+   answer2: ''
+ }
+ returnQues = {
+  question1: '',
+  answer1: ''
+}
+returns1Ques = {
+ question2: '',
+ answer2: ''
+}
+technicalQues = {
+  question1: '',
+  answer1: ''
+}
+technical1Ques = {
+ question2: '',
+ answer2: ''
+}
   refunds: string;
   return :string;
   technical :string;
@@ -36,27 +60,65 @@ export class FaqsPage implements OnInit {
   this. SecondQues.question2 = data.data().question2;
 
 })
-this.dbFaq.collection('FAQS').doc('Refunds').get().then((data) => {
+this.dbFaq.collection('FAQS').doc('Refunds').collection('Questions').doc('question1').get().then((data) => {
 
-  this.refunds = data.data().refunds;
-
-})
-this.dbFaq.collection('FAQS').doc('Returns').get().then((data) => {
-
-  this.return = data.data().returns;
+  this.refundQues.answer1 = data.data().answer;
+  this.refundQues.question1 = data.data().question1;
 
 })
-this.dbFaq.collection('FAQS').doc('Technical').get().then((data) => {
+this.dbFaq.collection('FAQS').doc('Refunds').collection('Questions').doc('question2').get().then((data) => {
 
-  this.technical = data.data().technical;
+this. refunds1Ques.answer2 = data.data().answer;
+this. refunds1Ques.question2 = data.data().question2;
 
 })
+this.dbFaq.collection('FAQS').doc('Returns').collection('Questions').doc('question1').get().then((data) => {
+
+  this.returnQues.answer1 = data.data().answer;
+  this.refundQues.question1 = data.data().question1;
+
+})
+this.dbFaq.collection('FAQS').doc('Returns').collection('Questions').doc('question2').get().then((data) => {
+
+this. returns1Ques.answer2 = data.data().answer;
+this. returns1Ques.question2 = data.data().question2;
+
+})
+this.dbFaq.collection('FAQS').doc('Technical').collection('Questions').doc('question1').get().then((data) => {
+
+  this.technicalQues.answer1 = data.data().answer;
+  this.technicalQues.question1 = data.data().question1;
+
+})
+this.dbFaq.collection('FAQS').doc('Technical').collection('Questions').doc('question2').get().then((data) => {
+
+this. technical1Ques.answer2 = data.data().answer;
+this. technical1Ques.question2 = data.data().question2;
+
+})
+// this.dbFaq.collection('FAQS').doc('Refunds').get().then((data) => {
+
+//   this.refunds = data.data().refunds;
+
+// })
+// this.dbFaq.collection('FAQS').doc('Returns').get().then((data) => {
+
+//   this.return = data.data().returns;
+
+// })
+// this.dbFaq.collection('FAQS').doc('Technical').get().then((data) => {
+
+//   this.technical = data.data().technical;
+
+// })
 
   }
+  
 dismiss(){
   this.modalController.dismiss({
     'dismissed':true
   });
 }
+
 
 }
