b99211292597a00fb91f3606279e99ed57b3a6f3
kiwokuhle <39264305+kiwokuhle@users.noreply.github.com>
2020-02-19 12:42:51 +0200

    updates

--- src/app/pages/faqs/faqs.page.ts
+++ src/app/pages/faqs/faqs.page.ts
@@ -182,12 +182,20 @@ Technical = [];
 // })
 
   }
+  val;
   searchresult(event) {
     // console.log(usersinput);
-    console.log(event.target.value);
-    this.filterItems(event.target.value, this.questions)
-    console.log(this.questions);
- console.log('serach arrayhere',this.searchArray);
+    this.val = event.target.value
+    if(this.val == '') {
+      this.val = ''
+    }else {
+      console.log(event.target.value);
+      this.filterItems(event.target.value, this.questions)
+      console.log(this.questions);
+   console.log('serach arrayhere',this.searchArray);
+    }
+   
+ 
     // if(this.searchArray[' '] === ' '){
     //   this.filterItems[' '] === ' ';
     // }
